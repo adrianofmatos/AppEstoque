657d6a289c7d1cf6f3d0b98cc34468869d762f59
Adriano Falcão <adrianofalcao@gmail.com>
2018-09-04 10:33:36 -0300

    Comitando as alterações

---
+++ src/main/java/com/algaworks/erp/model/TipoEmpresa.java
@@ -0,0 +1,20 @@
+package com.algaworks.erp.model;
+
+public enum TipoEmpresa {
+	
+	MEI("Microemprendedor Individual"),
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+
+	private TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
